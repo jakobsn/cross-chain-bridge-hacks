3c1407dd6f6fb77c7e04907f3218149ea71c082f
Jakob Notland <jakob.notland@ntnu.no>
2023-01-26 16:25:53 +0100

    Use dotenv to get ALCHEMY_API_KEY from .env. The old API key is now invalid.

--- anySwap/hardhat.config.js
+++ anySwap/hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -6,7 +7,7 @@ module.exports = {
   networks: {
     hardhat: {
       forking: {
-        url: "https://eth-mainnet.g.alchemy.com/v2/nViNxPZFReBvRdbyLMnU43iKp3E3Wlyc",
+        url: process.env.ALCHEMY_API_KEY,
         blockNumber: 13906273
       }
     }
